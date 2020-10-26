66b28461c2f5a943c32cd64e20e98534511df148
袁晓栋 <xiaodong.yuan@percent.cn>
2020-10-26 23:03:43 +0800

    Flutter 1.14.6 Dart 2.8.0

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/yuan/Documents/Flutter/sdk/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yuan/Documents/Flutter/Flutter_Learning_Collection"
-export "FLUTTER_TARGET=/Users/yuan/Documents/Flutter/Flutter_Learning_Collection/lib/main.dart"
+export "FLUTTER_ROOT=/Users/yuan/Documents/development/Flutter/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yuan/Documents/development/Flutter/Flutter_Learning_Collection"
+export "FLUTTER_TARGET=/Users/yuan/Documents/development/Flutter/Flutter_Learning_Collection/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/yuan/Documents/Flutter/sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yuan/Documents/development/Flutter/sdk/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
